7b21ab5eaa71c551a20900b45e6fe68a55f33776
Emanuel <130445385+UPEV1sion@users.noreply.github.com>
2024-09-03 09:17:43 +0200

    Added size

--- hashmap.h
+++ hashmap.h
@@ -11,62 +11,77 @@ typedef struct HashMap *HashMap;
 typedef size_t (*hash)(const void *key, size_t key_size);
 
 /**
- * @brief creates a HashMap
- * @param hm_capacity the initial capacity of the hashmap
- * @param key_size the sizeof value of the key
- * @param value_size the sizeof value of the value
+ * @brief Creates a new HashMap with a generic hash function.
+ *
+ * @param hm_capacity The initial capacity of the HashMap
+ * @param key_size The sizeof value of the key
+ * @param value_size The sizeof value of the value
  * @return HashMap
  */
 HashMap hm_create(size_t hm_capacity, size_t key_size, size_t value_size);
 
 /**
- * @brief creates a HashMap with a custom hash function
- * @param hm_capacity the initial capacity of the hashmap
- * @param key_size the sizeof value of the key
- * @param value_size the sizeof value of the value
- * @param hash_func the custom hash function
+ * @brief Creates a new HashMap with a custom hash function.
+ *
+ * @param hm_capacity The initial capacity of the HashMap
+ * @param key_size The sizeof value of the key
+ * @param value_size The sizeof value of the value
+ * @param hash_func The custom hash function
  * @return HashMap
  */
 HashMap hm_create_ch(size_t hm_capacity, size_t key_size, size_t value_size, hash hash_func);
 
 /**
- * @brief destroys the hashmap
- * @param hm the hashmap
- * @return success status
+ * @brief Destroys the HashMap.
+ *
+ * @param hm The HashMap
+ * @return Success code
  */
 int hm_destroy(HashMap hm);
 
 /**
- * @brief gets the value at the specified key
- * @param hm the hashmap
- * @param key the key
- * @return the value at the key
+ * @brief Retrieves the value at the specified key.
+ *
+ * @param hm The HashMap
+ * @param key The key
+ * @return The value at the key
  */
 void *hm_get(HashMap hm, const void *key);
 
 /**
- * @brief sets the value at the specified key
- * @param hm the hashmap
- * @param key the key
- * @param value the value
- * @return success status
+ * @brief Updates the value at the specified key.
+ *
+ * @param hm The HashMap
+ * @param key The key
+ * @param value The value
+ * @return Success code
  */
 int hm_set(HashMap hm, const void *key, const void *value);
 
 /**
- * @brief adds an item to the hashmap
- * @param hm the hashmap
- * @param key the key
- * @param value the value
- * @return success status
+ * @brief Adds a new key-value pair to the HashMap.
+ *
+ * @param hm The HashMap
+ * @param key The key
+ * @param value The value
+ * @return Success code
  */
 int hm_put(HashMap hm, const void *key, const void *value);
 
 /**
- * @brief removes an item from the hashmap
- * @param hm the hashmap
- * @param key the key
- * @return success status
+ * @brief Returns the size of the HashMap.
+ *
+ * @param hm The HashMap
+ * @return The size
+ */
+size_t hm_size(HashMap hm);
+
+/**
+ * @brief Removes the key-value pair from the HashMap
+ *
+ * @param hm The HashMap
+ * @param key The key
+ * @return Success code
  */
 int hm_remove(HashMap hm, const void *key);
 
